c23dbb1918dc923943f0918f07daa6c56b7ba3f6
Justin Lubin <jlubi333@gmail.com>
2020-04-23 14:50:35 -0400

    Add some io utils

--- lib/stdlib2/io2.ml
+++ lib/stdlib2/io2.ml
@@ -26,3 +26,15 @@ let read_file : string -> string =
       close_in_noerr channel;
       raise e
 
+let visible_files : string -> string list =
+  fun dir ->
+    dir
+      |> Sys.readdir
+      |> Array.to_list
+      |> List.filter
+           ( fun file ->
+               not (Sys.is_directory file)
+                 && String.length file > 0
+                 && String.get file 0 <> '.'
+           )
+      |> List.sort String.compare
--- lib/stdlib2/io2.mli
+++ lib/stdlib2/io2.mli
@@ -1,2 +1,5 @@
 val read_all : in_channel -> string
+
 val read_file : string -> string
+
+val visible_files : string -> string list
