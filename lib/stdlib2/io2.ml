8e7456bab739fc557e488b55ac048aa40741bc1f
Justin Lubin <jlubi333@gmail.com>
2020-05-02 14:16:30 -0400

    Better support for absolute paths

--- lib/stdlib2/io2.ml
+++ lib/stdlib2/io2.ml
@@ -22,10 +22,21 @@ let read_file : string -> string =
 
 let path : string list -> string =
   fun parts ->
-    parts
-      |> List2.concat_map (String.split_on_char '/')
-      |> List.filter (fun s -> not (String.equal s ""))
-      |> String.concat "/"
+    let standard =
+      parts
+        |> List2.concat_map (String.split_on_char '/')
+        |> List.filter (fun s -> not (String.equal s ""))
+        |> String.concat "/"
+    in
+    let combined =
+      parts
+        |> String.concat ""
+        |> String.trim
+    in
+    if String.length combined > 0 && combined.[0] = '/' then
+      "/" ^ standard
+    else
+      standard
 
 let read_path : string list -> string =
   fun parts ->
