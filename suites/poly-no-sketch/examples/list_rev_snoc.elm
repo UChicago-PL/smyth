b58df1d5aa23e4e9104e3a6225cdb532d04fbdba
Justin Lubin <jlubi333@gmail.com>
2020-05-10 23:54:03 -0400

    Fix some polymorphic benchmarks

--- suites/poly-no-sketch/examples/list_rev_snoc.elm
+++ suites/poly-no-sketch/examples/list_rev_snoc.elm
@@ -2,6 +2,6 @@ specifyFunction (listRevSnoc <Nat>)
   [ ([]<Nat>, []<Nat>)
   -- , ([0]<Nat>, [0]<Nat>)
   -- , ([1]<Nat>, [1]<Nat>)
-  , ([0, 1]<Nat>, [1, 0]<Nat>)
-  -- , ([0, 0, 1]<Nat>, [1, 0, 0]<Nat>)
+  -- , ([0, 1]<Nat>, [1, 0]<Nat>)
+  , ([0, 0, 1]<Nat>, [1, 0, 0]<Nat>)
   ]
