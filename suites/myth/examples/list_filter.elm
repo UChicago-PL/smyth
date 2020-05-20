e39890fe9e108671fc549d2cacb049493b27089d
Justin Lubin <jlubi333@gmail.com>
2020-05-19 23:17:40 -0400

    Fix examples

--- suites/myth/examples/list_filter.elm
+++ suites/myth/examples/list_filter.elm
@@ -5,6 +5,7 @@ specifyFunction2 listFilter
   , (isEven, [2], [2])
   , (isEven, [0, 0], [0, 0])
   , (isEven, [0, 1], [0])
+  , (isEven, [1, 0, 1], [0])
   , (isNonzero, [], [])
   , (isNonzero, [0], [])
   ]
