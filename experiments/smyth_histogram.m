d828a0b936397a1129844697b86bf4eb20ee996c
Justin Lubin <jlubi333@gmail.com>
2020-05-28 11:34:06 -0400

    Histogram creation

---
+++ experiments/smyth_histogram.m
@@ -0,0 +1,17 @@
+experiment = argv(){1};
+amount = argv(){2};
+
+nice_experiment = ['Experiment ' strsplit(experiment, '-'){2}]
+
+diffs = csvread(['data/' experiment '/histogram/k' amount '.csv']);
+
+horizontal = min(diffs):1:max(diffs);
+
+figure('visible', 'off')
+hist(diffs, horizontal, 'facecolor', [0, 0.4470, 0.7410])
+
+title([nice_experiment ', k_{' amount '} - k_{expert}'])
+xlabel('Difference')
+ylabel('Number of Tasks')
+
+saveas(gcf, ['data/' experiment '/histogram/k' amount '.png']);
--- experiments/smyth_plot.m
+++ experiments/smyth_plot.m
@@ -1,6 +1,8 @@
 experiment = argv(){1};
 name = argv(){2};
 
+nice_experiment = ['Experiment ' strsplit(experiment, '-'){2}]
+
 data = csvread(['data/' experiment '/csv/' name '.csv']);
 
 timeout = data(2, 2);
@@ -11,7 +13,7 @@ data = data(5:end,:);
 figure('visible', 'off')
 plot(data(:, 1), data(:, 2), 'LineWidth', 3);
 
-title([name ' (' experiment ', N = ' num2str(trials) ', timeout = ' num2str(timeout) 's)'], 'Interpreter', 'none');
+title([name ' (' nice_experiment ', N = ' num2str(trials) ', timeout = ' num2str(timeout) 's)'], 'Interpreter', 'none');
 
 xlim([0 inf])
 xlabel('Example Set Size');
