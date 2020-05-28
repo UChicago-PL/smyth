dda4582dd50b4aa57a16af81459f6ca71ae3609e
Justin Lubin <jlubi333@gmail.com>
2020-05-28 12:07:14 -0400

    Add some semicolons to the Octave files

--- experiments/smyth_histogram.m
+++ experiments/smyth_histogram.m
@@ -7,11 +7,11 @@ diffs = csvread(['data/' experiment '/histogram/k' amount '.csv']);
 
 horizontal = min(diffs):1:max(diffs);
 
-figure('visible', 'off')
-hist(diffs, horizontal, 'facecolor', [0, 0.4470, 0.7410])
+figure('visible', 'off');
+hist(diffs, horizontal, 'facecolor', [0, 0.4470, 0.7410]);
 
-title([nice_experiment ', k_{' amount '} - k_{expert}'])
-xlabel('Difference')
-ylabel('Number of Tasks')
+title([nice_experiment ', k_{' amount '} - k_{expert}']);
+xlabel('Difference');
+ylabel('Number of Tasks');
 
 saveas(gcf, ['data/' experiment '/histogram/k' amount '.png']);
--- experiments/smyth_plot.m
+++ experiments/smyth_plot.m
@@ -10,15 +10,15 @@ trials = data(3, 2);
 
 data = data(5:end,:);
 
-figure('visible', 'off')
+figure('visible', 'off');
 plot(data(:, 1), data(:, 2), 'LineWidth', 3);
 
 title([name ' (' nice_experiment ', N = ' num2str(trials) ', timeout = ' num2str(timeout) 's)'], 'Interpreter', 'none');
 
-xlim([0 inf])
+xlim([0 inf]);
 xlabel('Example Set Size');
 
-ylim([0 1])
+ylim([0 1]);
 ylabel('Success Percent');
 
 saveas(gcf, ['data/' experiment '/png/' name '.png']);
