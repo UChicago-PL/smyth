073dcc7a479fbab6ab43db8d565956f2d625e0d5
Justin Lubin <jlubi333@gmail.com>
2020-07-18 18:52:15 -0400

    Add script that Makefile depends on (fixes previous commit)

---
+++ misc-utils/modify-odoc-css.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd _build/default/_doc/_html
+
+echo '' >> odoc.css
+echo '/* Custom Modifications */' >> odoc.css
+echo '' >> odoc.css
+
+echo 'b { font-weight: 600; }' >> odoc.css
+echo 'em { font-style: normal; font-variant: small-caps; }' >> odoc.css
