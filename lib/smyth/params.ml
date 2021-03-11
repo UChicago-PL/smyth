e87f43e462d30be5224c7c59695a07ba8e45091d
Niek <n.mulleners@uu.nl>
2021-03-11 11:09:14 +0100

    Increase max_solution_count to compensate for previously missing solutions

--- lib/smyth/params.ml
+++ lib/smyth/params.ml
@@ -21,7 +21,7 @@ let ranking_method : ranking_method ref =
 (** The method to rank synthesis solutions. *)
 
 let max_solution_count : int option ref =
-  ref (Some 30)
+  ref (Some 40)
 (** The maximum synthesis solution count. [None] indicates no bound/infinity. *)
 
 let uneval_case_budget : int ref =
